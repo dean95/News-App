6e53bb2f42bb593d90c461e458b77bb87036987d
Dean Djermanović <dean74745@gmail.com>
2016-12-29 12:55:36 +0100

    Add click listener on list item to open website URL

--- app/src/main/java/com/example/dean/newsapp/News.java
+++ app/src/main/java/com/example/dean/newsapp/News.java
@@ -3,14 +3,8 @@ package com.example.dean.newsapp;
 public class News {
     private String title;
     private String section;
-    private String author;
     private String datePublished;
     private String webUrl;
-
-    public News(String title, String section) {
-        this.title = title;
-        this.section = section;
-    }
 
     public News(String title, String section, String datePublished, String webUrl) {
         this.title = title;
@@ -19,24 +13,12 @@ public class News {
         this.webUrl = webUrl;
     }
 
-    public News(String title, String section, String datePublished, String webUrl, String author) {
-        this.title = title;
-        this.section = section;
-        this.datePublished = datePublished;
-        this.webUrl = webUrl;
-        this.author = author;
-    }
-
     public String getTitle() {
         return title;
     }
 
     public String getSection() {
         return section;
-    }
-
-    public String getAuthor() {
-        return author;
     }
 
     public String getDatePublished() {
--- app/src/main/java/com/example/dean/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/dean/newsapp/NewsActivity.java
@@ -1,7 +1,11 @@
 package com.example.dean.newsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,8 +21,21 @@ public class NewsActivity extends AppCompatActivity {
 
         ArrayList<News> news = QueryUtils.extractNews();
 
-        NewsAdapter adapter = new NewsAdapter(this, news);
+        final NewsAdapter adapter = new NewsAdapter(this, news);
 
         newsListView.setAdapter(adapter);
+
+        newsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                News currentNews = adapter.getItem(position);
+
+                Uri newsUri = Uri.parse(currentNews.getWebUrl());
+
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, newsUri);
+                
+                startActivity(websiteIntent);
+            }
+        });
     }
 }
