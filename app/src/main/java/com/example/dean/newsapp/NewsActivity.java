123ee1758b0af1d5ab528efc0915f5dbf8089e6f
Dean Djermanović <dean74745@gmail.com>
2016-12-29 16:14:51 +0100

    Add settings activity

--- app/src/main/java/com/example/dean/newsapp/NewsActivity.java
+++ app/src/main/java/com/example/dean/newsapp/NewsActivity.java
@@ -8,6 +8,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -68,6 +70,23 @@ public class NewsActivity extends AppCompatActivity
 
             emptyStateView.setText(R.string.no_internet_connection);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        if(id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 
     @Override
